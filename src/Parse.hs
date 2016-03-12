8849f24f4845075dd9535db472e4f4373521d163
Basile Henry <basile.henry@hotmail.com>
2016-03-12 14:11:56 +0000

    Added Type signatures

--- src/Parse.hs
+++ src/Parse.hs
@@ -2,17 +2,21 @@ module Parse(parseAST) where
 
 import           AST
 import           Control.Monad      (void)
-import           Text.Parsec
+import           Text.Parsec        (ParseError (..), Parsec, char, digit,
+                                     endOfLine, eof, many, many1, noneOf, oneOf,
+                                     string, try, (<|>))
 import           Text.Parsec.String (parseFromFile)
 
 parseAST :: String -> IO (Either ParseError AST)
 parseAST = parseFromFile file
 
+file :: Parsec String u AST
 file = do
     result <- many line
     eof
     return result
 
+line :: Parsec String u Line
 line = do
     n <- many1 digit
     many1 $ char ' '
@@ -23,8 +27,10 @@ line = do
     void endOfLine <|> eof
     return $ Line (read n) s
 
+statement :: Parsec String u Statement
 statement = (try statementAgain) <|> (try statementDefer) <|> (try statementComma) <|> (try statementPrint) <|> statementDoExpr
 
+statementAgain :: Parsec String u Statement
 statementAgain = do
     string "again"
     many $ char ' '
@@ -35,6 +41,7 @@ statementAgain = do
     s <- statement
     return $ Again c s
 
+statementDefer :: Parsec String u Statement
 statementDefer = do
     string "defer"
     many $ char ' '
@@ -45,6 +52,7 @@ statementDefer = do
     s <- statement
     return $ Defer c s
 
+statementComma :: Parsec String u Statement
 statementComma = do
     d <- doExp
     many $ char ' '
@@ -53,6 +61,7 @@ statementComma = do
     s <- statement
     return $ Comma d s
 
+statementPrint :: Parsec String u Statement
 statementPrint = do
     string "print("
     many $ char ' '
@@ -61,12 +70,15 @@ statementPrint = do
     char ')'
     return $ Print s
 
+statementDoExpr :: Parsec String u Statement
 statementDoExpr = do
     d <- doExp
     return $ Statement d
 
+condition :: Parsec String u Condition
 condition = (try conditionOr) <|> (try conditionAnd) <|> (try conditionNot) <|> conditionExp
 
+conditionOr :: Parsec String u Condition
 conditionOr = do
     e <- expression
     many $ char ' '
@@ -75,6 +87,7 @@ conditionOr = do
     c <- condition
     return $ Or e c
 
+conditionAnd :: Parsec String u Condition
 conditionAnd = do
     e <- expression
     many $ char ' '
@@ -83,16 +96,19 @@ conditionAnd = do
     c <- condition
     return $ And e c
 
+conditionNot :: Parsec String u Condition
 conditionNot = do
     char '!'
     many $ char ' '
     e <- expression
     return $ Not e
 
+conditionExp :: Parsec String u Condition
 conditionExp = do
     e <- expression
     return $ Condition e
 
+expression :: Parsec String u Exp
 expression = (try $ exp2 AST.GT ">")
          <|> (try $ exp2 AST.LT "<")
          <|> (try $ exp2 AST.GE ">=")
@@ -101,6 +117,7 @@ expression = (try $ exp2 AST.GT ">")
          <|> (try $ exp2 AST.EQ "==")
          <|> expLExp
 
+exp2 :: (LExp -> LExp -> Exp) -> String -> Parsec String u Exp
 exp2 f s = do
     l1 <- lExp
     many $ char ' '
@@ -109,12 +126,15 @@ exp2 f s = do
     l2 <- lExp
     return $ f l1 l2
 
+expLExp :: Parsec String u Exp
 expLExp = do
     l <- lExp
     return $ Exp l
 
+doExp :: Parsec String u DoExp
 doExp = (try doExpHash) <|> doExpLExp
 
+doExpHash :: Parsec String u DoExp
 doExpHash = do
     f <- factor
     many $ char ' '
@@ -123,12 +143,15 @@ doExpHash = do
     l <- lExp
     return $ Hash f l
 
+doExpLExp :: Parsec String u DoExp
 doExpLExp = do
     l <- lExp
     return $ DoExp l
 
+lExp :: Parsec String u LExp
 lExp = (try lExpAdd) <|> (try lExpSub) <|> lExpTerm
 
+lExpAdd :: Parsec String u LExp
 lExpAdd = do
     t1 <- term
     many $ char ' '
@@ -137,6 +160,7 @@ lExpAdd = do
     t2 <- term
     return $ Add t1 t2
 
+lExpSub :: Parsec String u LExp
 lExpSub = do
     t1 <- term
     many $ char ' '
@@ -145,12 +169,15 @@ lExpSub = do
     t2 <- term
     return $ Sub t1 t2
 
+lExpTerm :: Parsec String u LExp
 lExpTerm = do
     t <- term
     return $ LExp t
 
+term :: Parsec String u Term
 term = (try termMul) <|> (try termDiv) <|> (try termMod) <|> termFact
 
+termMul :: Parsec String u Term
 termMul = do
     f1 <- factor
     many $ char ' '
@@ -159,6 +186,7 @@ termMul = do
     f2 <- factor
     return $ Mul f1 f2
 
+termDiv :: Parsec String u Term
 termDiv = do
     f1 <- factor
     many $ char ' '
@@ -167,6 +195,7 @@ termDiv = do
     f2 <- factor
     return $ Div f1 f2
 
+termMod :: Parsec String u Term
 termMod = do
     f1 <- factor
     many $ char ' '
@@ -175,12 +204,15 @@ termMod = do
     f2 <- factor
     return $ Mod f1 f2
 
+termFact :: Parsec String u Term
 termFact = do
     f <- factor
     return $ Term f
 
+factor :: Parsec String u Factor
 factor = (try factorPar) <|> (try factorRead) <|> (try factorNeg) <|> (try factorFunc) <|> factorInt
 
+factorPar :: Parsec String u Factor
 factorPar = do
     char '('
     many $ char ' '
@@ -189,18 +221,22 @@ factorPar = do
     char ')'
     return $ Par l
 
+factorRead :: Parsec String u Factor
 factorRead = do
     string "read()"
     return Read
 
+factorNeg :: Parsec String u Factor
 factorNeg = do
     char '-'
     many $ char ' '
     l <- lExp
     return $ Neg l
 
+factorFunc :: Parsec String u Factor
 factorFunc = (try $ funcFact N "N") <|> funcFact U "U"
 
+funcFact :: Function -> String -> Parsec String u Factor
 funcFact f n = do
     string $ n ++ "("
     many $ char ' '
@@ -209,12 +245,15 @@ funcFact f n = do
     char ')'
     return $ FuncFact f l
 
+factorInt :: Parsec String u Factor
 factorInt = do
     n <- many1 digit
     return $ Factor $ read n
 
+strs :: Parsec String u Strs
 strs = (try strConcat) <|> strStrs
 
+strConcat :: Parsec String u Strs
 strConcat = do
     s <- str
     many $ char ' '
@@ -223,27 +262,34 @@ strConcat = do
     ss <- strs
     return $ Concat s ss
 
+strStrs :: Parsec String u Strs
 strStrs = do
     s <- str
     return $ Strs s
 
+str :: Parsec String u Str
 str = (try strString) <|> (try $ funcStr U "U") <|> strLExp
 
+escape :: Parsec String u [Char]
 escape = do
     d <- char '\\'
     c <- oneOf "\\\"0nrvtbf" -- all the characters which can be escaped
     return [d, c]
 
+nonEscape :: Parsec String u Char
 nonEscape = noneOf "\\\"\0\n\r\v\t\b\f"
 
+character :: Parsec String u [Char]
 character = fmap return nonEscape <|> escape
 
+strString :: Parsec String u Str
 strString = do
     char '\"'
     cs <- many character
     char '\"'
     return $ Str $ concat cs
 
+funcStr :: Function -> String -> Parsec String u Str
 funcStr f n = do
     string $ n ++ "("
     many $ char ' '
@@ -252,6 +298,7 @@ funcStr f n = do
     char ')'
     return $ FuncStr f l
 
+strLExp :: Parsec String u Str
 strLExp = do
     l <- lExp
     return $ Num l
